4467a648373ecb4e8093b32137cacc47944fbdae
munnacoder1 <munna.inovation@gmail.com>
2025-04-27 12:14:11 +0600

    Enhance hello handler responses with status and message
    - Updated the Root and Hello handlers to include a message and status in the JSON response.
    - This change improves the clarity of the API responses, indicating service health and welcome messages.

--- go-microservice/services/jobs_service/rest_api/handlers/hello.go
+++ go-microservice/services/jobs_service/rest_api/handlers/hello.go
@@ -9,11 +9,15 @@ import (
 func (handlers *Handlers) Root(w http.ResponseWriter, r *http.Request) {
 	utils.SendJson(w, http.StatusOK, map[string]any{
 		"success": true,
+		"message": "Welcome to Jobs Service API v1",
+		"status":  "healthy",
 	})
 }
 
 func (handlers *Handlers) Hello(w http.ResponseWriter, r *http.Request) {
 	utils.SendJson(w, http.StatusOK, map[string]any{
 		"success": true,
+		"message": "Service is healthy and running 100%",
+		"status":  "healthy",
 	})
 }
